a65d872a1056fd20d06d7849f9f132959e240fd1
Chris LeCroy <cslecroy@gmail.com>
2021-04-07 22:11:21 -0700

    removed deprecation warning

--- Sources/SwiftUIMapView/MapView.swift
+++ Sources/SwiftUIMapView/MapView.swift
@@ -20,7 +20,6 @@ import UIKit
  - Copyright: 2020 Sören Gade
  */
 @available(iOS, introduced: 13.0)
-@available(iOS, deprecated: 14.0, message: "Please consider using the official Map view.")
 public struct MapView: UIViewRepresentable {
     
     // MARK: Properties
