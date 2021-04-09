6ee79e5458328124268d2b84f0497eeab94714eb
Chris LeCroy <cslecroy@gmail.com>
2021-04-09 15:34:07 -0700

    added displayPriority

--- Sources/SwiftUIMapView/Views/MapAnnotationView.swift
+++ Sources/SwiftUIMapView/Views/MapAnnotationView.swift
@@ -11,20 +11,19 @@ import MapKit
 
 /**
  Custom annotation view for `MapAnnotation` objects.
- 
+
  Sets the view's `glyphImage` and `markerTintColor` according to values of the annotation.
  Automatically takes advantage of clustering via an optionally set `clusteringIdentifier`.
  */
 class MapAnnotationView: MKMarkerAnnotationView {
-    
     override var annotation: MKAnnotation? {
         willSet {
             if let mapAnnotation = newValue as? MapViewAnnotation {
-                self.clusteringIdentifier = mapAnnotation.clusteringIdentifier
-                self.markerTintColor = mapAnnotation.tintColor
-                self.glyphImage = mapAnnotation.glyphImage
+                clusteringIdentifier = mapAnnotation.clusteringIdentifier
+                markerTintColor = mapAnnotation.tintColor
+                glyphImage = mapAnnotation.glyphImage
+                displayPriority = mapAnnotation.displayPriority ?? .defaultLow
             }
         }
     }
-    
 }
